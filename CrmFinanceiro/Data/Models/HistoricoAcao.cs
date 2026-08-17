87aaf66dfd385dd305c538c7bd22d0cfae889f9c
GIF26 <giovannappink26@gmail.com>
2026-08-17 16:33:08 -0300

    Adicionar arquivos de projeto.

---
+++ CrmFinanceiro/Data/Models/HistoricoAcao.cs
@@ -0,0 +1,29 @@
+﻿using System.Numerics;
+
+namespace CrmFinanceiro.Data.Models;
+
+public class HistoricoAcao
+{
+    protected HistoricoAcao() { }
+    public HistoricoAcao(FinanceiroCaixa titulo, Usuario usuario, DateTime dataAcao, int tipoAcao, string observacao)
+    {
+        Titulo = titulo;
+        Usuario = usuario;
+        DataAcao = DateTime.UtcNow;
+        TipoAcao = tipoAcao;
+        Observacao = observacao;
+    }
+
+    public long Id { get; private set; }
+
+    public int TituloId { get; private set; }
+    public FinanceiroCaixa Titulo { get; private set; } = null!;
+
+    public int UsuarioId { get; private set; }
+    public Usuario Usuario { get; private set; } = null!;
+
+    public DateTime DataAcao { get; private set; } 
+    public int TipoAcao { get; private set; }
+    public string Observacao{ get; set; } = string.Empty;
+
+}
